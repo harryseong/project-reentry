eec97ad151fec9f53caa76c43aa119304cf7bb84
Harry Seong <harryseong@gmail.com>
2020-05-24 18:57:18 -0500

    Added admin button to navbar

--- src/app/shared/components/nav/nav.component.ts
+++ src/app/shared/components/nav/nav.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Location} from '@angular/common';
+import {ActivatedRoute} from '@angular/router';
+import {BehaviorSubject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent implements OnInit {
+export class NavComponent implements OnInit, OnDestroy {
+  isAdminPage$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  activatedRouteSubscription$: Subscription;
 
-  constructor(private location: Location) { }
+  constructor(private location: Location,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.activatedRouteSubscription$ = this.route.parent.url.subscribe(rsp => {
+      this.isAdminPage$.next(rsp.toString() === 'admin');
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.activatedRouteSubscription$.unsubscribe();
   }
 
   goBack() {
